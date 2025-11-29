788adae7870775c9f4e959fd208cfb36a6b0d2aa
mariusbekken <marius.bekken@icloud.com>
2025-11-29 19:31:13 +0100

    feat: update footer links to point to the correct URLs for privacy, terms, and disclosure

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer">
         <p>&copy; IPRaven.com 2025 | All rights reserved.</p>
-        <p class="small"><a href="#" target="_blank">Privacy</a> | <a href="#" target="_blank">Terms</a> | <a href="#" target="_blank">Disclosure</a> | <a href="mailto:contact@ipraven.com">Contact</a></p>
+        <p class="small"><a href="https://privacy.ipraven.com" target="_blank">Privacy</a> | <a href="https://terms.ipraven.com" target="_blank">Terms</a> | <a href="https://disclosure.ipraven.com" target="_blank">Disclosure</a> | <a href="mailto:contact@ipraven.com">Contact</a></p>
     </footer>
 </template>
 
