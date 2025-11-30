1d55785e47cfbf6e7411fc0bae4465dbef676b09
mariusbekken <marius.bekken@icloud.com>
2025-11-30 16:08:19 +0100

    refactor: remove old footer component and related views
    - Deleted FooterComponentOld.vue as it was outdated.
    - Removed AboutView.vue, BlogView.vue, BlogPostView.vue, DisclosureView.vue, PrivacyView.vue, and TermsView.vue as they are no longer needed.
    - Cleaned up router configuration by removing routes associated with the deleted views.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import IndexView from '@/views/IndexView.vue'
-import HomeView from '../views/HomeView.vue'
-import BlogView from '@/views/BlogView.vue'
-import BlogPostView from '@/views/BlogPostView.vue'
-import PrivacyView from '@/views/PrivacyView.vue'
-import TermsView from '@/views/TermsView.vue'
-import AboutView from '@/views/AboutView.vue'
-import DisclosureView from '@/views/DisclosureView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,41 +13,6 @@ const router = createRouter({
       path: '/',
       name: 'index',
       component: IndexView,
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/blog',
-      name: 'blog',
-      component: BlogView,
-    },
-    {
-      path: '/blog/:slug',
-      name: 'blog-post',
-      component: BlogPostView,
-    },
-    {
-      path: '/privacy',
-      name: 'privacy',
-      component: PrivacyView,
-    },
-    {
-      path: '/terms',
-      name: 'terms',
-      component: TermsView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
-    },
-    {
-      path: '/disclosure',
-      name: 'disclosure',
-      component: DisclosureView,
     },
   ],
 })
