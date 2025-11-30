a037f79811062eb74c66f5930531c8cc6bd47aa1
mariusbekken <marius.bekken@icloud.com>
2025-11-30 16:23:10 +0100

    refactor: update README and footer links; modify page title for clarity; remove unused blog post files and HomeView component

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -4,7 +4,3 @@
         <p class="small"><a href="https://privacy.ipraven.com" target="_blank">Privacy</a> | <a href="https://terms.ipraven.com" target="_blank">Terms</a> | <a href="https://disclosure.ipraven.com" target="_blank">Disclosure</a> | <a href="mailto:contact@ipraven.com">Contact</a></p>
     </footer>
 </template>
-
-<script setup>
-
-</script>
