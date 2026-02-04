4c3fabf25558ebdf434509491e8451e2affcc171
mariusbekken <marius.bekken@icloud.com>
2026-02-04 18:06:09 +0100

    feat: Update footer to dynamically display the current year

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,13 @@
 <template>
     <footer class="footer">
-        <p data-nosnippet>&copy; IPRaven.com 2025 | All rights reserved.</p>
+        <p data-nosnippet>&copy; IPRaven.com {{ dateToday }} | All rights reserved.</p>
         <p class="small" data-nosnippet><a href="https://privacy.ipraven.com" target="_blank">Privacy</a> | <a href="https://terms.ipraven.com" target="_blank">Terms</a> | <a href="https://disclosure.ipraven.com" target="_blank">Disclosure</a> | <a href="mailto:contact@ipraven.com">Contact</a></p>
         <p class="footer-description">IPRaven helps you understand what information your browser and device reveal online, including IP, geolocation, headers and fingerprint data.</p>
     </footer>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const dateToday = ref(new Date().getFullYear())
+</script>
